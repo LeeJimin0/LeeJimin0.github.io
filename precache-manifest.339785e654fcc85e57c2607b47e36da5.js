9b7d2c47aabbecbc990e76102d47e8c3bb252ee0
LeeJimin0 <jimine0819@naver.com>
2020-02-19 20:08:37 +0900

    first commit

---
+++ precache-manifest.339785e654fcc85e57c2607b47e36da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981672f0dd8f292027be8efcca1ff09f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9927d63b45d97c2e633",
+    "url": "/static/css/main.e06f2546.chunk.css"
+  },
+  {
+    "revision": "0408ce7a0ff4c9e50fbd",
+    "url": "/static/js/2.ea5d773a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ea5d773a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9927d63b45d97c2e633",
+    "url": "/static/js/main.901c2a74.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
